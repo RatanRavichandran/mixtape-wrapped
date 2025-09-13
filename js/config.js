a9a55f10d2016c913755bdb09fee73716a4dfbef
Ratan Ravichandran <74362517+RatanRavichandran@users.noreply.github.com>
2025-09-13 21:33:28 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// Filled properly in a later step:
+export const SPOTIFY_CLIENT_ID = ""; // will set later
+export const REDIRECT_URI = window.location.origin + window.location.pathname; // your Pages URL
+export const SCOPES = ["user-top-read"]; // can add more later
+
+// Temporary playlist IDs (we’ll replace with yours later)
+export const PLAYLIST_SIDE_A = ""; 
+export const PLAYLIST_SIDE_B = "";
