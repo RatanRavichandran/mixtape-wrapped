fdf0567e8d41f003b9aaf95e6a4e12cb848909f2
Ratan Ravichandran <74362517+RatanRavichandran@users.noreply.github.com>
2025-09-13 21:46:27 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Filled properly in a later step:
-export const SPOTIFY_CLIENT_ID = ""; // will set later
-export const REDIRECT_URI = window.location.origin + window.location.pathname; // your Pages URL
+export const SPOTIFY_CLIENT_ID = "9001291498964c86a9cb338101b59a1e"; // will set later
+export const REDIRECT_URI = "https://ratanravichandran.github.io/mixtape-wrapped/"; // your Pages URL
 export const SCOPES = ["user-top-read"]; // can add more later
 
 // Temporary playlist IDs (we’ll replace with yours later)
